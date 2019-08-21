9d1fd3404368b39eb3f26b30fd2b620b4de2ef97
MarcosMurilo <m_murillomatos@yahoo.com.br>
2019-08-20 22:12:50 -0300

    inseri funcao emojis

--- CAÇA-NIQUEL/script.js
+++ CAÇA-NIQUEL/script.js
@@ -87,13 +87,13 @@ let bancoImg3 = [
         
          // SOMANDO OU SUBTRAINDO OS VALORES DO SALDO CONFORME RESULTADO DA ROLETA
          if ( ran1 == ran2 && ran1 == ran3 ){
-            if ( ran1 == 12 && ran2 == 12 && ran3 == 12) {
+           
                recebeRoleta = 150
-               recebeRoleta += 300
+            
             }
             
             
-       } else if (ran1 == ran2 && ran1 != ran3){
+        else if (ran1 == ran2 && ran1 != ran3){
 
          if (ran1 >= 0 && ran1 <= 2 && ran2 >= 0 && ran2 <=2) {
             recebeRoleta = 20
@@ -104,7 +104,7 @@ let bancoImg3 = [
          } else if ( ran1 >= 6 && ran1 <= 8 && ran2 >= 6 && ran2 <=8) {
             recebeRoleta = 20
             recebeRoleta += 15
-         } else if ( ran1 >= 9 && ran1 <= 11 && ran2 >= 9 && ran2 <= 11 ) {
+         } else if ( ran1 >= 9 && ran1 <= 12 && ran2 >= 9 && ran2 <= 12 ) {
             recebeRoleta = 20
             recebeRoleta += 20
          }
@@ -121,7 +121,7 @@ let bancoImg3 = [
          } else if ( ran1 >= 6 && ran1 <= 8 && ran3 >= 6 && ran3 <=8) {
             recebeRoleta = 20
             recebeRoleta += 15
-         } else if ( ran1 >= 9 && ran1 <= 11 && ran3 >= 9 && ran3 <= 11 ) {
+         } else if ( ran1 >= 9 && ran1 <= 12 && ran3 >= 9 && ran3 <= 12 ) {
             recebeRoleta = 20
             recebeRoleta += 20
          }
@@ -137,7 +137,7 @@ let bancoImg3 = [
          } else if ( ran2 >= 6 && ran2 <= 8 && ran3 >= 6 && ran3 <=8) {
             recebeRoleta = 20
             recebeRoleta += 15
-         } else if ( ran2 >= 9 && ran2 <= 11 && ran3 >= 9 && ran3 <= 11 ) {
+         } else if ( ran2 >= 9 && ran2 <= 12 && ran3 >= 9 && ran3 <= 12 ) {
             recebeRoleta = 20
             recebeRoleta += 20
          }
@@ -183,27 +183,29 @@ let bancoImg3 = [
 
   // função faz parar as fotos nos quadros
   function para() {
+
       if (recebeRoleta == 150) {
-
+       
          corSection.style.backgroundColor = "#0a4fe2" // cor fundo azul
-
+         let emojVariaTitulo = document.querySelector('h1').innerHTML = ganha() + "Jogo Caça Níquel" + ganha(); // chama função ganha para mostrar os emoji´s
       } else if(recebeRoleta >= 20 && recebeRoleta < 150) {
 
          corSection.style.backgroundColor = "#ffa500" // cor fundo amarela
-
+         let emojVariaTitulo = document.querySelector('h1').innerHTML = ganha() + "Jogo Caça Níquel" + ganha();
       } else {
          corSection.style.backgroundColor = "#c20c0c" // cor fundo vermelha
+         let emojVariaTitulo = document.querySelector('h1').innerHTML = perde() + "Jogo Caça Níquel" + perde();
       }
 
       var recebeInput = document.getElementById('credito').value // ID  input
-      var inputCredito = document.getElementById('credito') // adiciona o valor "somado" no vampo input
+      var inputCredito = document.getElementById('credito') // adiciona o valor "somado" no campo input
       var creditoSomado
-      saldo.innerHTML =''
+      saldo.innerHTML ='' // limpa o valor senão fica colocando os valores lado a lado sempre que aperta o botão "Parar"
       var credito = parseInt(recebeInput) // passa o value do input para 'int'
       
       let item = document.createElement('option') // texto  'Digite um numero....
       item.text = `SALDO $ ${ creditoSomado = credito + recebeRoleta}`// só texo na tela
-      
+      //let emojVariaTitulo = document.querySelector('h1').innerHTML = ganha() + "Jogo Caça Níquel" + perde();
       item.value =  credito // dispara msg de game over caso crédito fique 'zero'
       if (creditoSomado <= 0) {
          alert("GAME OVER!")
@@ -215,28 +217,23 @@ let bancoImg3 = [
       var para = clearInterval(rodandoRoleta)
       
   }
+  //mostra emoji´s felizes ao ganhar crédito
+  function ganha(){
+    // ARRAY FONTE      
+    let emoj = ["\u{1F913}","\u{1F973}","\u{1F911}","\u{1F619}","\u{1F929}", "\u{1F4B5}", "\u{1F61C}", "\u{1F91E}","\u{1F64F}","\u{2728}","\u{1F3C6}","\u{1F48E}"]//U+1F91E U+1F64F	 U+2728	U+1F3C6 U+1F48E
+    let ran =  Math.floor(Math.random() * emoj.length) 
+    return emoj[ran] 
+       
+}//fim ganha
+
+// mostra emoji´s tristes ao perder crédito
+function perde(){
+    // ARRAY FONTE      
+    let emoj = ["\u{1F92E}","\u{1F630}","\u{1F62D}","\u{1F623}","\u{1F97A}","\u{1F4B8}","\u{1F621}","\u{1F631}","\u{1F624}","\u{1F92C}","\u{1F480}","\u{1F4A9}"]// U+1F631  U+1F624 U+1F92C	 U+1F480 U+1F4A9
+    let ran =  Math.floor(Math.random() * emoj.length) 
+    return emoj[ran] 
   
-
-/*
-let emoj = [
-    "img/img-001.jpeg",
-    "img/img-002.jpeg",
-    "img/img-003.jpeg",
-    "img/img-004.jpeg",
-    "img/img-005.jpeg",
-    "img/img-006.jpeg",
-    "img/img-007.jpeg",
-    "img/img-008.jpg",
-    "img/img-009.jpg",
-    "img/img-010.jpeg",
-    "img/img-011.jpg",
-    "img/img-012.jpg",
-    "img/img-013.jpg"
-]
-
-
-*/
-
+}//fim perde
 
 
 
